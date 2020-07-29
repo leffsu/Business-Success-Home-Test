e3a0e63ed0e44adf95deb43c2ef7a08f945722fb
Lev Nazarov <leff@leff.su>
2020-07-29 18:06:11 +0300

    А звонки и открытие браузера я сделать-то и забыл!

--- app/src/main/java/su/leff/businesssuccesshometest/ui/contactinfo/ContactInfoFragment.kt
+++ app/src/main/java/su/leff/businesssuccesshometest/ui/contactinfo/ContactInfoFragment.kt
@@ -1,10 +1,23 @@
 package su.leff.businesssuccesshometest.ui.contactinfo
 
+import android.Manifest
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.navigation.fragment.findNavController
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.android.synthetic.main.element_avatar.*
 import kotlinx.android.synthetic.main.element_profile_info.view.*
 import kotlinx.android.synthetic.main.fragment_contactinfo.*
@@ -45,8 +58,48 @@ class ContactInfoFragment : BaseFragment() {
             elementPhone.txvBody.text = user.phone
             elementPhone.txvDescription.text = getString(R.string.phone)
 
+            elementPhone.onClick {
+                Dexter.withContext(requireContext())
+                    .withPermission(Manifest.permission.CALL_PHONE)
+                    .withListener(object : PermissionListener {
+                        override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                            startActivity(
+                                Intent(
+                                    Intent.ACTION_CALL,
+                                    Uri.parse("tel:${user.phone}")
+                                )
+                            )
+                        }
+
+                        override fun onPermissionRationaleShouldBeShown(
+                            p0: PermissionRequest?,
+                            p1: PermissionToken?
+                        ) {
+                            println(p0)
+                        }
+
+                        override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                            println(p0)
+                        }
+
+                    }).check()
+            }
+
             elementWebsite.txvBody.text = user.websiteUrl
             elementWebsite.txvDescription.text = getString(R.string.website)
+
+            elementWebsite.onClick {
+                try {
+                    CustomTabsIntent.Builder().build()
+                        .launchUrl(requireContext(), Uri.parse("https://${user.websiteUrl}/"))
+                } catch (e: ActivityNotFoundException) {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.check_website),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
 
             btnEditContact.onClick {
                 val action =
